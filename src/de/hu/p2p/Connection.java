04723ea743ef1ed26652c15bbce63671593877b8
melcher <melcher@fzi.de>
2019-07-11 01:32:52 +0200

    testing

--- src/de/hu/p2p/Connection.java
+++ src/de/hu/p2p/Connection.java
@@ -3,6 +3,7 @@ package de.hu.p2p;
 import javax.json.Json;
 import javax.json.JsonObject;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
@@ -13,17 +14,23 @@ public class Connection extends Thread {
     private Socket socket;
     private PrintWriter pw;
     private String ip;
+    private BufferedReader br;
+
     public Connection(Socket socket, Servent servent){
         this.servent = servent;
         this.socket = socket;
         this.ip=(((InetSocketAddress) socket.getRemoteSocketAddress()).getAddress()).toString().replace("/","");
+        try {
+            this.pw = new PrintWriter(socket.getOutputStream(), true);
+            this.br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run(){
         try{
             // Creates a new BufferedReader listening to new messages from the command line
-            BufferedReader br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-            this.pw = new PrintWriter(socket.getOutputStream(), true);
             while(true) {
                 JsonObject jo = Json.createReader(br).readObject();
                 if(jo.containsKey("username")){
